7693279e09ea6ec7b2692bd4d921fc3f44d325c4
KeigoMega <31650689+KeigoMega@users.noreply.github.com>
2020-12-16 08:38:46 +0900

    change Structure of Directory

--- build_by_py2exe32.bat
+++ build_by_py2exe32.bat
@@ -1,4 +1,4 @@
 python setup.py
-move .\dist\main.exe .\dist\QR4CAD_32.exe
-rm -R QR4CAD
-move dist QR4CAD
+move .\dist\main.exe .\dist\QR4CAD.exe
+rm -R QR4CAD_32
+move dist QR4CAD_32
--- build_by_py2exe64.bat
+++ build_by_py2exe64.bat
@@ -1,4 +1,4 @@
 python setup.py
-move .\dist\main.exe .\dist\QR4CAD_64.exe
-rm -R QR4CAD
-move dist QR4CAD
+move .\dist\main.exe .\dist\QR4CAD.exe
+rm -R QR4CAD_64
+move dist QR4CAD_64
